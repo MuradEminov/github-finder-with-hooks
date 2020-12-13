c93011fd952b36a43186c05c3250f18186653ad1
MuradEminov <murademinov88@gmail.com>
2020-12-13 16:35:03 +0400

    Search and Users components moved inside Home component. NotFound component is created to be rendered for the non-existing Route paths

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import React, { Fragment } from "react";
+import React from "react";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
+import Home from "../src/components/pages/Home";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "../src/components/layout/Alert";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/pages/About";
 import GithubState from "./context/github/GithubState";
 import AlertState from "../src/context/github/alert/AlertState";
+import NotFound from "../src/components/pages/NotFound";
 
 const App = () => {
   return (
@@ -20,18 +20,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
